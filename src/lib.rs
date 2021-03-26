6188d7350878296b4d6af0e0daf8125af04ca23b
Tamme Schichler <tamme@schichler.dev>
2021-03-26 10:22:07 +0100

    Added remaining HTML attributes

--- src/lib.rs
+++ src/lib.rs
@@ -73,6 +73,8 @@ macro_rules! attributes {
 	{$namespace:ident=>
 		$(
 			$(-$(-$deprecated:tt)?)?
+			$(*$(*$experimental:tt)?)?
+			$(!$(!$obsolete:tt)?)?
 			$name:ident on
 			$([$(
 				$(-$(-$deprecated_impl:tt)?)?
@@ -82,8 +84,19 @@ macro_rules! attributes {
 		),*$(,)?
 	} => {$(
 		$(
-			#[deprecated = "TODO"]
+			#[deprecated = "This deprecated API should no longer be used, but will probably still work."]
+			/// `deprecated`
 			$(compile_error!($deprecated))?
+		)?
+		$(
+			#[deprecated = "This is an experimental API that should not be used in production code."]
+			/// `experimental`
+			$(compile_error!($experimental))?
+		)?
+		$(
+			#[deprecated = "This is an obsolete API and is no longer guaranteed to work."]
+			/// `obsolete`
+			$(compile_error!($obsolete))?
 		)?
 		pub struct $name;
 		#[allow(deprecated)]
@@ -106,6 +119,7 @@ macro_rules! attributes {
 			impl crate::$namespace::$element for $name {}
 		)*)?
 		$(
+			#[allow(deprecated)]
 			impl crate::$namespace::GlobalAttribute for $name {}
 			$(compile_error!($global_marker))?
 		)?
@@ -152,7 +166,7 @@ pub mod html {
 	void_elements!(col);
 	// Forms
 	elements!(
-		button, datalist, fieldset, form, label, legend, meter, optgroup, option, progress, select,
+		button, datalist, fieldset, form, label, legend, meter, optgroup, option, output, progress, select,
 		textarea
 	);
 	void_elements!(input);
@@ -173,18 +187,19 @@ pub mod html {
 		-isindex, -keygen, -menuitem, -nextid, -spacer
 	);
 
+	/// See <https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes>.
 	pub mod attributes {
 		use super::Sealed;
 
 		attributes! {html=>
 			accept on [input, -form],
-			//accept_charset on [form],
+			// accept_charset on [form],
 			accesskey on all,
 			action on [form],
 			align on [applet, caption, col, colgroup, hr, iframe, img, table, tbody, td, tfoot, th, thead, tr],
 			allow on [iframe],
 			alt on [applet, area, img, input],
-			//r#async on [script],
+			// r#async on [script],
 			autocapitalize on all,
 			autocomplete on [form, input, select, textarea],
 			autofocus on [button, input, keygen, select, textarea],
@@ -203,6 +218,111 @@ pub mod html {
 			codebase on [applet],
 			-color on [basefont, font, hr],
 			cols on [textarea],
+			colspan on [td, th],
+			content on [meta],
+			contenteditable on all,
+			contextmenu on all,
+			controls on [audio, video],
+			coords on [area],
+			crossorigin on [audio, img, link, script, video],
+			*csp on [iframe],
+			data on [object],
+			//data-* on all,
+			datetime on [del, ins, time],
+			decoding on [img],
+			default on [track],
+			defer on [script],
+			dir on all,
+			dirname on [input, textarea],
+			disabled on [button, command, fieldset, input, keygen, optgroup, option, select, textarea],
+			download on [a, area],
+			draggable on all,
+			enctype on [form],
+			*enterkeyhint on all, // <textarea>, contenteditable
+			// r#for on [label, output],
+			form on [button, fieldset, input, keygen, label, meter, object, output, progress, select, textarea],
+			formaction on [input, button],
+			formenctype on [button, input],
+			formmethod on [button, input],
+			formnovalidate on [button, input],
+			formtarget on [button, input],
+			headers on [td, th],
+			height on [canvas, embed, iframe, img, input, object, video], // and deprecated `on all`, but this can't be expressed without min_specialization.
+			hidden on all,
+			high on [meter],
+			href on [a, area, base, link],
+			hreflang on [a, area, link],
+			// http_equiv on [meta],
+			icon on [command],
+			id on all,
+			*importance on [iframe, img, link, script],
+			integrity on [link, script],
+			-intrinsicsize on [img],
+			inputmode on all, // <textarea>, contenteditable
+			ismap on [img],
+			itemprop on all,
+			keytype on [keygen],
+			kind on [track],
+			label on [optgroup, option, track],
+			lang on all,
+			-language on [script],
+			*loading on [img, iframe],
+			list on [input],
+			// r#loop on [audio, bgsound, marquee, video],
+			low on [meter],
+			!manifest on [html],
+			max on [input, meter, progress],
+			maxlength on [input, textarea],
+			minlength on [input, textarea],
+			media on [a, area, link, source, style],
+			method on [form],
+			min on [input, meter],
+			multiple on [input, select],
+			muted on [audio, video],
+			name on [button, form, fieldset, iframe, input, keygen, object, output, select, textarea, map, meta, param],
+			novalidate on [form],
+			open on [details],
+			optimum on [meter],
+			pattern on [input],
+			ping on [a, area],
+			placeholder on [input, textarea],
+			poster on [video],
+			preload on [audio, video],
+			radiogroup on [command],
+			readonly on [input, textarea],
+			referrerpolicy on [a, area, iframe, img, link, script],
+			rel on [a, area, link],
+			required on [input, select, textarea],
+			reversed on [ol],
+			rows on [textarea],
+			rowspan on [td, th],
+			sandbox on [iframe],
+			scope on [th],
+			!scoped on [style],
+			selected on [option],
+			shape on [a, area],
+			size on [input, select],
+			sizes on [link, img, source],
+			slot on all,
+			span on [col, colgroup],
+			spellcheck on all,
+			src on [audio, embed, iframe, img, input, script, source, track, video],
+			srcdoc on [iframe],
+			srclang on [track],
+			srcset on [img, source],
+			start on [ol],
+			step on [input],
+			style on all,
+			-summary on [table],
+			tabindex on all,
+			target on [a, area, base, form],
+			title on all,
+			translate on all,
+			// r#type on [button, input, command, embed, object, script, source, style, menu],
+			usemap on [img, input, object],
+			value on [button, data, input, li, meter, option, progress, param],
+			width on [canvas, embed, iframe, img, input, object, video], // and deprecated `on all`, but this can't be expressed without min_specialization.
+			wrap on [textarea],
 		}
 	}
 }
